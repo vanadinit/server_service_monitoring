2589b861210a1cb827b53e23f73dcecbdd47bc86
Johannes Paul <vanadinit@quantentunnel.de>
2022-11-20 20:36:27 +0100

    Refactor vpnstatus

--- vpnstatus_app.py
+++ vpnstatus_app.py
@@ -11,7 +11,8 @@
 
 import os
 
-def html_output(status, statusline, clients):
+
+def html_output(status, status_line, clients):
     htmltext = """<!DOCTYPE html>
 <html>
     <head>
@@ -22,13 +23,13 @@ def html_output(status, statusline, clients):
     <body>
         <h1>VPN-Server-Status</h1>
         <p>Serverstatus:<br/>
-            <span class="active" {statusstyle}>{statusline}</span><br/>
+            <span class="active" {status_style}>{status_line}</span><br/>
         </p>
         <table>
             <thead>
                 <tr><th>Nutzername</th><th>VPN-Addresse</th><th>Reale Addresse</th><th>Gesendet</th><th>Empfangen</th><th>Verbunden seit</th></tr>
             </thead>
-            <tbody>{clientlist}
+            <tbody>{client_list}
             </tbody>
         </table>
     </body>
@@ -36,30 +37,20 @@ def html_output(status, statusline, clients):
     """
 
     if status == 'aktiv':
-        statusstyle = 'style="color:#00FF00"'
+        status_style = 'style="color:#00FF00"'
     elif status == 'inaktiv':
-        statusstyle = 'style="color:#FF0000"'
+        status_style = 'style="color:#FF0000"'
     else:
-        statusstyle = 'class="fehler"'
+        status_style = 'class="fehler"'
 
-    clientlist = ''
-    clienttempl =  '\n                <tr><td>{cn}</td><td>{virt}</td><td>{real}</td><td>{recv}</td><td>{sent}</td><td>{since}</td></tr>'
-    for client in clients:
-        clientlist += clienttempl.format(
-            cn=client['cn'],
-            virt=client['virt'],
-            real=client['real'],
-            recv=client['recv'],
-            sent=client['sent'],
-            since=client['since'],
-        )
+    client_templ = '<tr><td>{cn}</td><td>{virt}</td><td>{real}</td><td>{recv}</td><td>{sent}</td><td>{since}</td></tr>'
+    client_list = '\n                '.join([client_templ.format(**client) for client in clients])
 
-    htmltext = htmltext.format(
-        statusstyle=statusstyle,
-        statusline=statusline,
-        clientlist=clientlist,
+    return htmltext.format(
+        status_style=status_style,
+        status_line=status_line,
+        client_list=client_list,
     )
-    return htmltext
 
 
 def vpnstatus(environ, start_response):
@@ -67,16 +58,16 @@ def vpnstatus(environ, start_response):
     sys.path.append(os.path.dirname(os.path.abspath(__file__)))
     import vpnstatus_parser
 
-    status, statusline = vpnstatus_parser.get_service_info()
+    status, status_line = vpnstatus_parser.get_service_info()
 
     clients = []
     if status == 'aktiv':
-        clients = vpnstatus_parser.get_status_info(True)
+        clients = vpnstatus_parser.get_status_info(tel=True)
 
     # For Debugging
     #    print(clients, file=environ['wsgi.errors'])
 
-    htmltext = html_output(status, statusline, clients)
+    htmltext = html_output(status, status_line, clients)
     output = str.encode(htmltext)
     status = '200 OK'
     headers = [('Content-type', 'text/html'),
--- vpnstatus_parser.py
+++ vpnstatus_parser.py
@@ -5,6 +5,7 @@ import telnetlib
 # Path to logfile (format -> status-version 3)
 STATUSFILE = "/tmp/ramdisk/openvpn-status.log"
 
+
 def curses_header(screen, tel):
     screen.addstr("\n\n")
     if tel:
@@ -41,23 +42,28 @@ def curses_clients(screen, clients):
         "----------------------------------------------------------------------------------------------------------\n")
     screen.addstr("\n".join([fmt % h for h in clients]), curses.color_pair(4) | curses.A_BOLD)
 
+
 def get_service_info():
-    process = subprocess.Popen('systemctl status openvpn@server.service', shell=True, stdout=subprocess.PIPE, universal_newlines=True)
-    sysstatus = process.stdout.read()
-    statuslines = sysstatus.split('\n')
+    process = subprocess.Popen(
+        'systemctl status openvpn@server.service',
+        shell=True,
+        stdout=subprocess.PIPE,
+        universal_newlines=True,
+    )
 
     status = 'inaktiv'
-    statusline = "Fehler in der Statusüberprüfung!!!\n"
-    for line in statuslines:
+    status_line = 'Fehler in der Statusüberprüfung!!!\n'
+    for line in process.stdout.read().split('\n'):
         if line.find('Active') > -1:
             if line.find('inactive') == -1:
                 status = 'aktiv'
-            statusline = line
+            status_line = line
             break
 
-    return status, statusline
+    return status, status_line
 
-def get_status_info(tel):
+
+def get_status_info(tel: bool):
     clients = []
 
     if tel:
@@ -79,29 +85,26 @@ def get_status_info(tel):
     for line in stats:
         cols = line.split('\t')
         if len(cols) == 12 and line.startswith('CLIENT_LIST'):
-            client = {}
-            client['cn'] = cols[1]
-            client['real'] = cols[2].split(':')[0]
-            client['virt'] = cols[3]
-            client['recv'] = byte2str(int(cols[5]))
-            client['sent'] = byte2str(int(cols[6]))
-            client['since'] = cols[7].strip()
-            clients.append(client)
-
+            clients.append({
+                'cn': cols[1],
+                'real': cols[2].split(':')[0],
+                'virt': cols[3],
+                'recv': byte2str(int(cols[5])),
+                'sent': byte2str(int(cols[6])),
+                'since': cols[7].strip(),
+            })
     return clients
 
 
 def byte2str(size):
     # Nice byte formatting
-    sizes = [
+    for border, suffix in [
         (1 << 50, 'PB'),
         (1 << 40, 'TB'),
         (1 << 30, 'GB'),
         (1 << 20, 'MB'),
         (1 << 10, 'KB'),
         (1, 'B')
-    ]
-    for f, suf in sizes:
-        if size >= f:
-            break
-    return "%.2f %s" % (size / float(f), suf)
+    ]:
+        if size >= border:
+            return f'{size / float(border):.2f} {suffix}'
